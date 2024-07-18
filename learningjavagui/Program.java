c6cb2f59472a2de24c540690581f955c9699dd84
KongRuksiam Studio <kongruksiamstudio@gmail.com>
2024-07-18 08:06:15 +0700

    upload project

---
+++ learningjavagui/Program.java
@@ -0,0 +1,43 @@
+package learningjavagui;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Program {
+
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame();
+//        frame.setTitle("My Program");
+//        frame.setSize(600, 400);
+//        frame.setResizable(false);
+//        //layout
+//        frame.setLayout(new FlowLayout());
+//        //label
+//        JLabel lb = new JLabel();
+//        lb.setText("Learning Java GUI");
+//        //button
+//        JButton btn = new JButton("Click");
+//        //event
+//        btn.addActionListener((ActionEvent e) -> {
+//            String name = JOptionPane.showInputDialog("Input Your Name");
+//            if(name!=null){
+//                if(name.isEmpty()){
+//                    JOptionPane.showMessageDialog(null, "Invalid Name","Alert", JOptionPane.ERROR_MESSAGE);
+//                }else{
+//                    JOptionPane.showMessageDialog(null,"Hello : "+name,"Alert", JOptionPane.INFORMATION_MESSAGE);
+//                }
+//                
+//            }
+//            
+//        });
+//        //add component
+//        frame.add(lb);
+//        frame.add(btn);
+//        frame.setLocationRelativeTo(null);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
+//    }
+
+}
